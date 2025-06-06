80ce6f34625b2b3b91731b9685e4bff02fb382c2
KatsaWins <chr.katsaros99@gmail.com>
2025-06-06 12:30:49 +0300

    Force push all local changes to master

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
-import HeaderIMG from '../assets/HeaderIMG.webp';
+import HeaderIMGL from '../assets/HeaderIMGL.webp';
+import HeaderIMGR from '../assets/HeaderIMGR.webp';
 import { homeText } from './HomePageText';
 
 function Home() {
   return (
     <>
       <div className="header-container">
-        {/* Background image */}
+        {/* Split Backgrounds */}
         <div
-          className="header-bg"
-          style={{ backgroundImage: `url(${HeaderIMG})` }}
-          data-aos="fade-in"
+          className="header-bg-left"
+          style={{ backgroundImage: `url(${HeaderIMGL})` }}
+          data-aos="fade-right" 
+        />
+        <div
+          className="header-bg-right"
+          style={{ backgroundImage: `url(${HeaderIMGR})` }}
+          data-aos="fade-left"
         />
 
         {/* Περιεχόμενο μόνο για desktop */}
         <div className="header-content">
           <div className="header-titles-row">
             <div className="header-left-block" data-aos="fade-right" data-aos-delay="200">
-              <h1 className="header-left-title">{"<h1> Hello World! </h1>"}</h1>
+              <h1 className="header-left-title">{" Hello World! "}</h1>
               <p className="home-paragraph" data-aos="fade-up" data-aos-delay="600">
                 {homeText}
               </p>
             </div>
 
             <div className="header-right-block" data-aos="fade-left" data-aos-delay="400">
-              <h1 className="header-right-title">{"<p> About me </p>"}</h1>
+              <h1 className="header-right-title">{"About me "}</h1>
               <p className="home-paragraph-right" data-aos="fade-up" data-aos-delay="900">
                 View my CV Here!
               </p>
               <a
-                 href="/cv/ChristosKatsaros_CV.pdf"
-                  target="_blank"
-                 rel="noopener noreferrer"
-                  className="cv-download-button"
-                >
-                         View CV
+                href="/cv/ChristosKatsaros_CV.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cv-download-button"
+              >
+                View CV
               </a>
-
             </div>
           </div>
         </div>
@@ -44,20 +49,21 @@ function Home() {
 
       {/* Περιεχόμενο μόνο για mobile */}
       <div className="header-mobile-content">
-      <h1 className="header-left-title">
-  <span > Hello </span>
-  <span>World! </span>
-</h1>
-
-        <p className="home-paragraph" data-aos="fade-down" data-aos-delay="1000" >{homeText}</p>
-               <a
-                href="/cv/ChristosKatsaros_CV.pdf"
-                 target="_blank"
-                 rel="noopener noreferrer"
-                 className="cv-download-button"
-                >
-                View CV
-               </a>
+        <h1 className="header-left-title"data-aos="fade-up" data-aos-delay="300" >
+          <span>Hello </span>
+          <span>World!</span>
+        </h1>
+        <p className="home-paragraph" data-aos="fade-down" data-aos-delay="1000">
+          {homeText}
+        </p>
+        <a
+          href="/cv/ChristosKatsaros_CV.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cv-download-button"
+        >
+          View CV
+        </a>
       </div>
     </>
   );
