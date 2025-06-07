5bc5211577ba94e62a48e7dc2a5f365232f191ab
KatsaWins <chr.katsaros99@gmail.com>
2025-06-07 13:57:19 +0300

    first commit

--- src/components/HomePageText.js
+++ src/components/HomePageText.js
@@ -1,6 +1,6 @@
 // TextContent.js
 const homeText = " ❝ Born in Athens in ’99. Former economics student turned web developer. Driven by creativity and the urge to build. Learning fast, growing faster—always exploring new technologies ❞"
-const homeText1 =`I'm a self-taught front-end developer passionate about crafting clean, user-friendly interfaces. With a solid foundation in HTML, CSS, JavaScript, and React, I'm constantly exploring new tools and design trends. I thrive on creative problem-solving and am eager to keep learning and growing in web development."
+const homeText1 =`I'm a self-taught front-end developer passionate about crafting clean, user-friendly interfaces. With a solid foundation in HTML, CSS, JavaScript, and React, I'm constantly exploring new tools and design trends. I thrive on creative problem-solving and I'm eager to keep learning and growing in web development."
 
 
 `
